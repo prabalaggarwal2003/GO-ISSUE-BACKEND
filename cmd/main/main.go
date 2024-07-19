89c409d327885b897eaca075f3cace62b50c3963
Aryan Pillai <aryanpillai518@gmail.com>
2024-07-19 21:41:08 +0530

    main changed again

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -5,13 +5,24 @@ import (
 	"net/http"
 
 	"github.com/aryan/go-issue-backend/pkg/routes"
+
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+
+	"os"
 )
 
 func main() {
+
 	r := mux.NewRouter()
 	routes.RegisterIssueRoutes(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:9010", r))
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "3000"
+	}
+
+	log.Fatal(http.ListenAndServe("0.0.0.0:", r))
 }
