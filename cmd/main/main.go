cbc1498a660eb8ede64af1f9e52b76bc0041a146
Aryan Pillai <aryanpillai518@gmail.com>
2024-07-19 23:14:13 +0530

    main file changed again

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -8,15 +8,12 @@ import (
 
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
-
-	
 )
 
 func main() {
 
 	r := mux.NewRouter()
 	routes.RegisterIssueRoutes(r)
-	http.Handle("/", r)
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
