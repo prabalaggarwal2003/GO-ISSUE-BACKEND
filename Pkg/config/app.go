5074b56a6bb3c2d259947ce162dcd869116093b6
Aryan Pillai <aryanpillai518@gmail.com>
2024-07-20 14:20:33 +0530

    app main changed

--- Pkg/config/app.go
+++ Pkg/config/app.go
@@ -12,7 +12,7 @@ var (
 func Connect(){
 
 
-	d, err := gorm.Open("mysql://unv1fcsdpjrssiks:G5TzMLd5Y7TmJHnp4k3e@biqvusjvq0ih4w4ikcw3-mysql.services.clever-cloud.com:3306/biqvusjvq0ih4w4ikcw3")
+	d, err := gorm.Open("mysql://","unv1fcsdpjrssiks:G5TzMLd5Y7TmJHnp4k3e@biqvusjvq0ih4w4ikcw3-mysql.services.clever-cloud.com:3306/biqvusjvq0ih4w4ikcw3")
 	if err != nil{
 		panic(err)
 	}
