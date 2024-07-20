82228be051176ce437812d90769335dc4e1ac65d
Aryan Pillai <aryanpillai518@gmail.com>
2024-07-20 14:39:13 +0530

    package installed and app changed

--- Pkg/config/app.go
+++ Pkg/config/app.go
@@ -1,24 +1,23 @@
 package config
 
-import(
+import (
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
 
 var (
-	db * gorm.DB
+	db *gorm.DB
 )
 
-func Connect(){
+func Connect() {
 
-
-	d, err := gorm.Open("mysql://","unv1fcsdpjrssiks:G5TzMLd5Y7TmJHnp4k3e@biqvusjvq0ih4w4ikcw3-mysql.services.clever-cloud.com:3306/biqvusjvq0ih4w4ikcw3")
-	if err != nil{
+	d, err := gorm.Open("mysql://", "unv1fcsdpjrssiks:G5TzMLd5Y7TmJHnp4k3e@biqvusjvq0ih4w4ikcw3-mysql.services.clever-cloud.com:3306/biqvusjvq0ih4w4ikcw3")
+	if err != nil {
 		panic(err)
 	}
 	db = d
 }
 
-func GetDB() *gorm.DB{
+func GetDB() *gorm.DB {
 	return db
 }
--- go.mod
+++ go.mod
@@ -1,7 +1,9 @@
 module github.com/aryan/go-issue-backend
+
 go 1.16
 
 require (
+	github.com/go-sql-driver/mysql v1.8.1 // indirect
 	github.com/gorilla/mux v1.8.0 // indirect
 	github.com/jinzhu/gorm v1.9.16 // indirect
 )
